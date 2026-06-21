b3e85c72eda778e4b3b5cf89be9c716d095686f9
M.Ibrahim <muhammadibrahimmi2005@gmail.com>
2026-06-21 23:10:41 +0500

    Day 4: Add IGenericRepository interface

---
+++ src/EmployeeLeaveMS.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeLeaveMS.Domain.Common;
+
+namespace EmployeeLeaveMS.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
