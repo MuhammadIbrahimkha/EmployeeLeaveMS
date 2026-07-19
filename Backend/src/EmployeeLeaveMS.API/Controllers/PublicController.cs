d86efd297108810a29e64c0be2eeedf25abad547
M.Ibrahim <muhammadibrahimmi2005@gmail.com>
2026-07-19 11:06:05 +0500

    Add PublicController with public departments endpoint for register page

---
+++ Backend/src/EmployeeLeaveMS.API/Controllers/PublicController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeLeaveMS.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeLeaveMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/public")]
+    public class PublicController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public PublicController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet("departments")]
+        public async Task<IActionResult> GetDepartments()
+        {
+            var result = await _adminService.GetAllDepartmentsAsync();
+            return Ok(result);
+        }
+    }
+}
